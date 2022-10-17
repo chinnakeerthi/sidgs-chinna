d6093ed8de5239198b87a814ee09183e3cabb039
chinnakeerthi <emailkeerthichinna52@gmail.com>
2022-10-17 19:57:31 +0530

    chinna dev push code application.java

--- application.java
+++ application.java
@@ -10,3 +10,29 @@ public class Alphabet {
             System.out.println(c + " is not an alphabet.");
     }
 }
+
+
+
+#new changes
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+        System.out.println(num + " is " + evenOdd);
+
+    }
+}[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[A[3~[3~[3~[3~[3~[3~[A[A[C[C[B[A[B
+
+
+
+
+[A[A
